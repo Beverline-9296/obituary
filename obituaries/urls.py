735c92d06de982eb8887bd0c3d2849184f50aeb5
Beverline-9296 <144214962+Beverline-9296@users.noreply.github.com>
2024-07-22 11:58:32 +0300

    Delete obituaries directory

--- obituaries/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('submit/', views.submit_obituary, name='submit_obituary'),
-    path('obituaries/', views.view_obituaries, name='view_obituaries'),
-]
